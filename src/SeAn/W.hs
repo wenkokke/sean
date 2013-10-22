1577e8f1f34b81e56b2ec3e3c62c21dd83c962ba
Wen Kokke <wenkokke@users.noreply.github.com>
2013-10-22 20:37:57 +0200

    took all positive aspects from the w2 implementation, solved part of the issue (no ambiguous types) and left it at "horrible death of the program if you ever _use_ an ambiguous function in the definition of another one" whereas you'd of course like to have "using ambiguous function in definition results in ambiguous definition"
    Signed-off-by: Pepijn Kokke <wen.kokke@gmail.com>

--- src/SeAn/W.hs
+++ src/SeAn/W.hs
@@ -18,144 +18,226 @@ import Control.Monad.Supply (Supply,supply,evalSupply)
 import Control.Monad.Trans (lift)
 import Control.Applicative ((<$>))
 
+type NeedsFreshNames a = Supply Name a
+type WithTypeErrors a  = Either TypeError a
+type WithErrors a      = Either ProgError a
+type W a               = ErrorT ProgError (Supply Name) a
+
 -- |Runs algorithm W on a list of declarations, making each previous
 --  declaration an available expression in the next.
-runW :: [Decl] -> Either TypeError TyEnv
-runW = refreshAll . withFreshNames . foldl addDecl (return mempty)
+inferTypes :: Prog -> WithErrors TyEnv
+inferTypes (Prog decls) = refreshAllW (supplyFreshNamesW (foldl addType (return []) decls))
   where
-  addDecl :: W TyEnv -> Decl-> W TyEnv
-  addDecl env (Decl x e) = do env <- env;
-                              (t,_) <- w (env,e);
-                              return (M.insert x t env)
+  addType :: W TyEnv -> Decl-> W TyEnv
+  addType env (Decl n exp)
+    = do env <- env ;
+         (t , _) <- inferType exp env ;
+         return (env << (n , t))
+
+-- |Implementation of algorithm W.
+inferType :: Expr -> TyEnv -> W (Type , TySubst)
+inferType exp env = case exp of
+
+  Con n t     -> return (t , Nil)
+
+  Var n       -> case findByName n env of
+                    (t:_) -> return (t , Nil)
+                    [   ] -> throwError (UnboundVariable n)
+
+  Abs x e     -> do a <- freshW;
+                    (t1 , s1) <- inferType e $ env << (x , a);
+                    return (TyArr (apply s1 a) t1 , s1)
+
+  App f x     -> do (t1 , s1) <- inferType f $ env
+                    (t2 , s2) <- inferType x $ applyEnv s1 env
+                    a  <- freshW
+                    s3 <- unifyW exp (apply s2 t1) (TyArr t2 a)
+                    return (apply s3 a , fromList [s1,s2,s3])
+
+  Let x e1 e2 -> do (t1 , s1) <- inferType e1 $ env
+                    (t2 , s2) <- inferType e2 $ applyEnv s1 env << (x , t1)
+                    return (t2 , fromList [s1,s2])
+
+-- |Lifting of `unify` to the inference monad.
+unifyW :: Expr -> Type -> Type -> W TySubst
+unifyW exp t1 t2 = case unify t1 t2 of
+  Left  e -> throwError (TypeErrorIn exp e)
+  Right s -> return s
+
+-- |Lifting of `supplyFreshNames` to the inference monad.
+supplyFreshNamesW :: W a -> WithErrors a
+supplyFreshNamesW m = evalSupply (runErrorT m) freshNames
+
+-- |Lifting of `refreshAll` to the inference monad.
+refreshAllW :: WithErrors TyEnv -> WithErrors TyEnv
+refreshAllW env = env >>= return . refreshAll
+
+
+-- * Type Environments
+
+type TyEnv = [(Name,Type)]
+
+-- |Inserts an entry into a type environment.
+(<<) :: TyEnv -> (Name,Type) -> TyEnv
+(<<) env (n,t) = (n,t) : env
+
+-- |Replaces an entry in a type environment (based on name equality).
+(<<!) :: TyEnv -> (Name,Type) -> TyEnv
+(<<!) env (n,t) = (n,t) : filter ((n /=) . fst) env
+
+-- |Applies a function to all types in a type environment.
+mapEnv :: (Type -> Type) -> TyEnv -> TyEnv
+mapEnv f = map (\(n,t) -> (n,f t))
+
+-- |Finds all entries for a given name in a type environment.
+findByName :: Name -> TyEnv -> [Type]
+findByName n = map snd . filter ((n ==) . fst)
+
+
+
+-- * Fresh Names
+
+-- |Generates a fresh type variables.
+fresh :: NeedsFreshNames Type
+fresh = do x <- supply; return (TyVar x)
+
+-- |Lifting of `fresh` to the inference monad.
+freshW :: W Type
+freshW = lift fresh
+
+-- |Provides an infinite stream of names to things in the @W@ monad,
+--  reducing it to just an @Either@ value containing perhaps a TypeError.
+supplyFreshNames :: NeedsFreshNames a -> a
+supplyFreshNames m = evalSupply m freshNames
+
+-- |Simply stream of fresh names.
+freshNames :: [Name]
+freshNames = letters ++ numbers
+  where
+  letters    = fmap (: []) (['A'..'D'] ++ ['F'..'S'] ++ ['U'..'Z'])
+  numbers    = fmap (('T' :) . show) [0..]
+
+-- |Replaces every type variable with a fresh one.
+refresh :: Type -> Type
+refresh = supplyFreshNames . lazyRefresh
+  where
+  -- |Lazily replace every type variable with a fresh one.
+  lazyRefresh :: Type -> NeedsFreshNames Type
+  lazyRefresh t = do subs <- forM (ftv t)
+                             $ \a ->
+                               do b <- fresh;
+                                  return (a ~> b)
+                     return (apply (fromList subs) t)
+
+-- |Refreshes all entries in a type environment.
+refreshAll :: TyEnv -> TyEnv
+refreshAll = mapEnv refresh
+
+
+
+-- * Type Substitutions
+
+data TySubst
+  = Nil
+  | Snoc Name Type TySubst
+  deriving (Eq,Show)
+
+-- |Performs a single substitution.
+for :: Name -> Type -> Type -> Type
+for x t' c@(TyCon _) = c
+for x t' v@(TyVar y) = if x == y then t' else v
+for x t' (TyArr a b) = TyArr (for x t' a) (for x t' b)
+
+-- |Applies a substitution to a type.
+apply :: TySubst -> Type -> Type
+apply Nil t = t
+apply (Snoc x t' s) t = for x t' (apply s t)
+
+-- |Applies a substitution to a type environment.
+applyEnv :: TySubst -> TyEnv -> TyEnv
+applyEnv s env = mapEnv (apply s) env
+
+-- |An alias for the creation of substitutions.
+(~>) :: Name -> Type -> TySubst
+(~>) x t' = Snoc x t' Nil
+
+-- |An alias for substitution concatination.
+andThen :: TySubst -> TySubst -> TySubst
+andThen Nil s2 = s2
+andThen (Snoc x t' s1) s2 = Snoc x t' (s1 `andThen` s2)
+
+-- |An alias for substitution sequencing.
+fromList :: [TySubst] -> TySubst
+fromList = foldl andThen Nil
+
+
+
+-- * Type Unifications
+
+-- |Unification as per Robinson's unification algorithm.
+unify :: Type -> Type -> WithTypeErrors TySubst
+unify t1@(TyCon a) t2@(TyCon b)
+    | a == b         = return Nil
+    | otherwise      = throwError (CannotUnify t1 t2)
+unify (TyArr a1 b1) (TyArr a2 b2)
+                     = do
+                       s1 <- unify a1 a2
+                       s2 <- unify (apply s1 b1) (apply s1 b2)
+                       return (s1 `andThen` s2)
+unify ty (TyVar x)
+    | x `occurs` ty = throwError (OccursCheck x ty)
+    | otherwise     = return (x ~> ty)
+unify (TyVar x) ty
+    | x `occurs` ty = throwError (OccursCheck x ty)
+    | otherwise     = return (x ~> ty)
+unify t1 t2         = throwError (CannotUnify t1 t2)
+
+-- |Occurs check for Robinson's unification algorithm.
+occurs :: Name -> Type -> Bool
+occurs n t = n `elem` (ftv t)
 
 -- |Returns the set of free type variables in a type.
 ftv :: Type -> [Name]
 ftv (TyCon   _) = [ ]
 ftv (TyVar   n) = [n]
 ftv (TyArr a b) = L.union (ftv a) (ftv b)
-  
-type TySubst = Map Name Type
 
--- |Substitutes a type for a type variable in a type.
-subst :: TySubst -> Type -> Type
-subst m c@(TyCon n) = c
-subst m v@(TyVar n) = M.findWithDefault v n m
-subst m (TyArr a b) = TyArr (subst m a) (subst m b)
 
-type TyEnv = Map Name Type
 
--- |Representation for possible errors in algorithm W.
+-- * Errors in Type Unification
+
+-- |Representation for possible errors in unification.
 data TypeError
-  = UnknownConstant Name                -- ^ thrown when unknown constant is encountered
-  | UnboundVariable Name                -- ^ thrown when unbound variable is encountered
-  | OccursCheck     Name Type           -- ^ thrown when occurs check in unify fails
-  | CannotUnify     Type Type           -- ^ thrown when types cannot be unified
-  | CannotUnifyIn   Type Type Expr Expr -- ^ thrown when types of expressions cannot be unified
-  | OtherError      String              -- ^ stores miscellaneous errors
+  = OccursCheck   Name Type -- ^ thrown when occurs check in unify fails
+  | CannotUnify   Type Type -- ^ thrown when types cannot be unified
+  | MiscTypeError String    -- ^ stores miscellaneous errors
   deriving Eq
 
 instance Error TypeError where
-  strMsg msg = OtherError msg
+  strMsg msg = MiscTypeError msg
 
 instance Show TypeError where
-  show (UnknownConstant n)
-    = printf "Unknown constant '%s'" n
-  show (UnboundVariable n)
-    = printf "Unbound variable '%s'" n
-  show (OccursCheck n  t1)
-    = printf "Occurs check fails; '%s' occurs in '%s'" n (show t1)
-  show (CannotUnify t1 t2)
-    = printf "Cannot unify '%s' with '%s'" (show t1) (show t2)
-  show (CannotUnifyIn t1 t2 e1 e2)
-    = printf "Cannot unify '%s' with '%s' in (%s %s)" (show t1) (show t2) (show e1) (show e2)
-  show (OtherError msg) = msg
+  show (OccursCheck n  t1) = printf "Occurs check fails; '%s' occurs in '%s'" n (show t1)
+  show (CannotUnify t1 t2) = printf "Cannot unify '%s' with '%s'" (show t1) (show t2)
+  show (MiscTypeError msg) = msg
 
-type W a = ErrorT TypeError (Supply Name) a
 
--- |Occurs check for Robinson's unification algorithm.
-occurs :: Name -> Type -> Bool
-occurs n t = n `elem` (ftv t)
 
--- |Unification as per Robinson's unification algorithm.
-unify :: Type -> Type -> W TySubst
-unify t1@(TyCon a) t2@(TyCon b)
-  | a == b        = return mempty
-  | otherwise     = throwError (CannotUnify t1 t2)
-unify (TyArr a1 b1) (TyArr a2 b2)
-                  = do
-                    s1 <- unify a1 a2
-                    s2 <- unify (subst s1 b1) (subst s1 b2)
-                    return (s2 <> s1)
-unify t1 (TyVar n)
-  | n `occurs` t1 = throwError (OccursCheck n t1)
-  | otherwise     = return (M.singleton n t1)
-unify (TyVar n) t2
-  | n `occurs` t2 = throwError (OccursCheck n t2)
-  | otherwise     = return (M.singleton n t2)
-unify t1 t2       = throwError (CannotUnify t1 t2)
+-- * Errors in Type Inference
 
--- |An alias for in the insertion of substitutions.
-(~>) :: Name -> Type -> TyEnv -> TyEnv
-(~>) = M.insert
+-- |Representation for possible errors in algorithm W.
+data ProgError
+  = UnknownConstant Name           -- ^ thrown when unknown constant is encountered
+  | UnboundVariable Name           -- ^ thrown when unbound variable is encountered
+  | TypeErrorIn     Expr TypeError -- ^ thrown when types of expressions cannot be unified
+  | MiscProgError   String         -- ^ stores miscellaneous errors
+  deriving Eq
 
--- |Implementation of algorithm W.
-w :: (TyEnv,Expr) -> W (Type , TySubst)
-w (env,exp) = case exp of
-  Con n t     -> return (t , mempty)
-  Var n       -> case M.lookup n env of
-                    Just  t -> return (t , mempty)
-                    Nothing -> throwError (UnboundVariable n)
-  
-  Abs   x e   -> do a <- fresh;
-                    (t1 , s1) <- w ((x ~> a) env, e);
-                    return (TyArr (subst s1 a) t1 , s1)
-  
-  App f   e   -> do (t1 , s1) <- w (env , f);
-                    (t2 , s2) <- w (subst s1 <$> env,e);
-                    a <- fresh;
-                    
-                    let s3_no_info = unify (subst s2 t1) (TyArr t2 a)
-                    let s3_with_info = handleAddExpr f e s3_no_info
-                    
-                    s3 <- s3_with_info
-                    return (subst s3 a , s3 <> (subst s3 <$> (s1 <> s2)))
-  
-  Let x e1 e2 -> do (t1 , s1) <- w (env , e1);
-                    (t2 , s2) <- w ((x ~> t1) (subst s1 <$> env) , e2);
-                    return (t2 , s2 <> s1)
+instance Error ProgError where
+  strMsg msg = MiscProgError msg
 
-handleAddExpr :: Expr -> Expr -> W a -> W a
-handleAddExpr e1 e2 m = m `catchError` addExpr e1 e2
-
-addExpr :: Expr -> Expr -> TypeError -> W a
-addExpr e1 e2 (CannotUnify t1 t2) = throwError (CannotUnifyIn t1 t2 e1 e2)
-
--- * Fresh Names
-
--- |Generates a fresh type in the W monad.
-fresh :: W Type
-fresh = do x <- lift supply;
-           return (TyVar x)
-
--- |Provides an infinite stream of names to things in the @W@ monad,
---  reducing it to just an @Either@ value containing perhaps a TypeError.
-withFreshNames :: W a -> Either TypeError a
-withFreshNames x = evalSupply (runErrorT x) freshNames
-  where
-  freshNames = letters ++ numbers
-    where
-    letters = fmap (: []) (['A'..'D'] ++ ['F'..'S'] ++ ['U'..'Z'])
-    numbers = fmap (('T' :) . show) [0..]
-
--- |Replaces every type variable with a fresh one.
-refresh :: Type -> W Type
-refresh t1 = do subs <- forM (ftv t1)
-                        $ \a ->
-                          do b <- fresh;
-                             return (M.singleton a b)
-                return (subst (mconcat subs) t1)
-                              
--- |Refreshes all entries in a type environment.
-refreshAll :: Either TypeError TyEnv -> Either TypeError TyEnv
-refreshAll env = do env <- env;
-                    M.mapM (withFreshNames . refresh) env
+instance Show ProgError where
+  show (UnknownConstant n)     = printf "Unknown constant '%s'" n
+  show (UnboundVariable n)     = printf "Unbound variable '%s'" n
+  show (TypeErrorIn exp err) = printf "Type error in %s; %s" (show exp) (show err)
+  show (MiscProgError msg)     = msg
